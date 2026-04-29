605487845e1d4972f0621b862ea9d8a32b3f9664
borisdubinin <borisdubinin01@gmail.com>
2026-04-29 22:40:28 +0300

    naming at IbanGenerator

--- src/main/java/org/example/util/IbanGenerator.java
+++ src/main/java/org/example/util/IbanGenerator.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class IbanGenerator {
 
-    private static final String PREFIX = "IBAN";
-    private static final int RANDOM_PART_LENGTH = 28;
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String IBAN_PREFIX = "IBAN";
+    private static final int RANDOM_IBAN_PART_LENGTH = 28;
+    private static final String POSSIBLE_IBAN_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final Random RANDOM = new Random();
 
     public static String generateIban() {
-        StringBuilder randomPart = new StringBuilder(RANDOM_PART_LENGTH);
-        for (int i = 0; i < RANDOM_PART_LENGTH; i++) {
-            int index = RANDOM.nextInt(CHARACTERS.length());
-            randomPart.append(CHARACTERS.charAt(index));
+        StringBuilder randomPart = new StringBuilder(RANDOM_IBAN_PART_LENGTH);
+        for (int i = 0; i < RANDOM_IBAN_PART_LENGTH; i++) {
+            int index = RANDOM.nextInt(POSSIBLE_IBAN_CHARACTERS.length());
+            randomPart.append(POSSIBLE_IBAN_CHARACTERS.charAt(index));
         }
-        return PREFIX + randomPart;
+        return IBAN_PREFIX + randomPart;
     }
 }
